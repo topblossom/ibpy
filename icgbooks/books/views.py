0cc7bd7db8c8edd647b4cd98a37a5da8a796fd8f
Jacek Palczewski <jpalczewski@gmail.com>
2020-05-01 21:44:13 +0200

    fix(books): style fixes

--- icgbooks/books/views.py
+++ icgbooks/books/views.py
@@ -1,6 +1,4 @@
-from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework import permissions
 from icgbooks.books.models import Book
 from icgbooks.books.serializers import BookSerializer
 
@@ -8,4 +6,3 @@ from icgbooks.books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
