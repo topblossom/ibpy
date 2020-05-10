d148dab60c87dae5d47c18fd383c44b47f8fbeac
Jacek Palczewski <jpalczewski@gmail.com>
2020-05-10 12:12:27 +0200

    fix(shelf): fixes travis errors

--- icgbooks/shelves/models.py
+++ icgbooks/shelves/models.py
@@ -4,7 +4,7 @@ from icgbooks.users.models import User
 
 class Shelf(models.Model):
     title = models.TextField()
-    owner = models.ForeignKey(User, on_delete = models.CASCADE)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
     books = models.ManyToManyField(Book)
 
     created_at = models.DateTimeField(auto_now_add=True)
