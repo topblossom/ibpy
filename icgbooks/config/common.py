caeb2e5b14cec54418a94fe251e068711b169697
Jacek Palczewski <jpalczewski@gmail.com>
2020-05-21 14:19:26 +0200

    feat: loose cors

--- icgbooks/config/common.py
+++ icgbooks/config/common.py
@@ -20,7 +20,7 @@ class Common(Configuration):
         'django.contrib.staticfiles',
 
         # Third party apps
-        'corsheaders'
+        'corsheaders',
         'rest_framework',  # utilities for rest apis
         'rest_framework.authtoken',  # token authentication
         'django_filters',  # for filtering rest endpoints
