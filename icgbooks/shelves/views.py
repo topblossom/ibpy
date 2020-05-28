8ad8642ea0bee42a53c1639eaa666cc746d08868
Jacek Palczewski <jpalczewski@gmail.com>
2020-05-28 23:51:10 +0200

    fix: shelf returns now only shelves owned by user. (#23)
    * fix: shelf returns now only shelves owned by user.
    
    * ci: heroku-only config
    
    * fix:  daily heroku fixes
    
    * fix:  daily heroku fixes

--- icgbooks/shelves/views.py
+++ icgbooks/shelves/views.py
@@ -3,5 +3,9 @@ from .models import Shelf
 from .serializers import ShelfSerializer
 
 class ShelfViewSet(viewsets.ModelViewSet):
+    def get_queryset(self):
+        user = self.request.user
+        return Shelf.objects.filter(owner=user)
+
     queryset = Shelf.objects.all()
     serializer_class = ShelfSerializer
