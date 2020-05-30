03dbe859432b42eeff93068c5823bb3570df1d2c
Jacek Palczewski <jpalczewski@gmail.com>
2020-05-30 11:23:32 +0200

    fix(shelf): add a title to shelf serializer, closes #27

--- icgbooks/shelves/serializers.py
+++ icgbooks/shelves/serializers.py
@@ -5,5 +5,5 @@ from icgbooks.shelves.models import Shelf
 class ShelfSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shelf
-        fields = ('id', 'owner', 'books', 'created_at', 'modified_at')
-        read_only_fields = ['created_at', 'modified_at']
+        fields = ('id', 'owner', 'books', 'title', 'created_at', 'modified_at')
+        read_only_fields = ('id','created_at', 'modified_at', 'owner')
